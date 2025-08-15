0e160895e730c67f600742fa12b2006093edba7d
maxuel henrique paula campos <124100812+MaxxArtes@users.noreply.github.com>
2025-08-15 01:20:25 +0000

    import

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from .database import Base
+# backend/models.py
+try:
+    from .database import Base
+except ImportError:
+    from database import Base
+
 
 class User(Base):
     __tablename__ = "users"
